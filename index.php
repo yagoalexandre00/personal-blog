a4e42eab277741de5c9d645113a9b0270ef9755d
Yago Alexandre <goltarayago@gmail.com>
2022-09-01 01:08:12 -0300

    primeiro commit: adicionando pastas

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include_once 'templates/header.php' ?>
+
+<?php include_once 'templates/footer.php' ?>
---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once 'config/url.php';
+echo $BASE_URL;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Pessoal</title>
+</head>
+
+<body>
