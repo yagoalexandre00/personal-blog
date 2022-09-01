c94ee02f92e66996a3a6b5cc256c01cb1d89ba3a
Yago Alexandre Goltara Affonso <yago.affonso@tce.ma.gov.br>
2022-09-01 12:15:24 -0300

    fix:editing favicon image

--- templates/header.php
+++ templates/header.php
@@ -18,8 +18,8 @@
 
 <body>
 
-    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-        <a class="navbar-brand" href="<?= $BASE_URL?>index.php">Home</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="<?= $BASE_URL ?>index.php"><img src="<?= $BASE_URL ?>img/favicon.png" alt="Code Icon"></a>
 
         <div class="navbar-nav">
             <a class="nav-link" href="#">Sobre</a>
