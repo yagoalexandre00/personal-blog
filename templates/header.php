ddae12724ef9b3e8b218f2d64a9561c58809cc83
Yago Alexandre Goltara Affonso <yago.affonso@tce.ma.gov.br>
2022-09-01 11:26:29 -0300

    feat:add navbar

--- templates/header.php
+++ templates/header.php
@@ -6,8 +6,24 @@
 <head>
     <meta charset="UTF-8">
     <title>Blog Pessoal</title>
-    <link rel="stylesheet" href="<?= $BASE_URL?>style/style.css">
-
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
 </head>
 
 <body>
+
+    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+        <a class="navbar-brand" href="<?= $BASE_URL?>index.php">Home</a>
+
+        <div class="navbar-nav">
+            <a class="nav-link" href="#">Sobre</a>
+            <a class="nav-link" href="#">Posts</a>
+            <a class="nav-link" href="#">Contatos</a>
+        </div>
+    </nav>
